b87f35345c680c5970c779cf790449812e6648bb
Supawit.JT <supawit.jt@gmail.com>
2026-01-24 14:14:52 +0700

    Last Commit

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-# --- 2. Custom CSS (GUI Redesign: Vivid, Modern & Friendly) ---
+# Custom CSS (GUI Redesign: Vivid, Modern & Friendly)
 st.markdown("""
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;600;800&display=swap');
